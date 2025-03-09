7f8f1452a9e4dd0c7342bb2113dfc12f86259597
patman <27874300+patmanf@users.noreply.github.com>
2025-03-08 23:38:30 -0600

    cleanup a little
    i mostly just wanted to remove that "require" at the top. but it doesnt work without it & i cant be bothered to figure out why. game sucks

--- pat/incineratefarmanimals.lua
+++ pat/incineratefarmanimals.lua
@@ -1,33 +1,38 @@
 require "/scripts/actions/monsters/farmable.lua"
 
-function dropMonsterHarvest(...)
-	script.setUpdateDelta(5)
-	
-	monster.setDropPool(nil)
-	
-	status.addEphemeralEffect("burning", math.huge)
-	status.addEphemeralEffect("melting", math.huge)
-	
-	for i = 1, 10 do
-		world.spawnProjectile(
-			"molotovflame",
-			entity.position(),
-			entity.id(),
-			vec2.rotate({1, 0}, util.toRadians(math.random(1, 360))),
-			false,
-			{power = 0}
-		)
-	end
-	
-	while status.resource("health") > 0 do
-		status.applySelfDamageRequest({
-			damageType = "IgnoresDef",
-			damage = status.resourceMax("health") / 25,
-			damageSourceKind = "fire",
-			sourceEntityId = entity.id()
-		})
-		coroutine.yield()
-	end
-	
-	return true
+function dropMonsterHarvest()
+  monster.setDropPool(nil)
+
+  status.addEphemeralEffect("burning", math.huge)
+  status.addEphemeralEffect("melting", math.huge)
+
+  local pos = entity.position()
+  local id = entity.id()
+  local params = {power = 0}
+
+  for i = 1, 10 do
+    local angle = math.pi * 2 * math.random()
+    local vec = {math.cos(angle), math.sin(angle)}
+    world.spawnProjectile("molotovflame", pos, id, vec, false, params)
+  end
+
+  local timer = 0
+  local req = {
+    damageType = "IgnoresDef",
+    damageSourceKind = "fire",
+    damage = status.resourceMax("health") / 25,
+    sourceEntityId = id,
+  }
+
+  while status.resource("health") > 0 do
+    timer = timer - script.updateDt()
+    if timer <= 0 then
+      timer = 0.08
+      status.applySelfDamageRequest(req)
+    end
+
+    coroutine.yield()
+  end
+
+  return true
 end
